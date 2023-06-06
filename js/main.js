a4892bd88871b33f544caf57cade4d2873d735f9
tanluc <tanluc1200@users.noreply.github.com>
2023-06-06 17:28:08 +0700

    update commit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+// go to top header
+function goToTop() {
+    const body = document.querySelector("body");
+    let scrollPosition;
+    const go_to_top = document.querySelector(".go-to-top");
+    window.addEventListener("scroll", () => {
+        scrollPosition =
+            window.pageYOffset || document.documentElement.scrollTop;
+        // const go_to_top = document.querySelector(".go-to-top");
+        if (scrollPosition >= 200) {
+            go_to_top.classList.add("active");
+        } else {
+            go_to_top.classList.remove("active");
+        }
+    });
+    go_to_top.addEventListener("click", () => {
+        document.documentElement.scrollTop = 0;
+    });
+}
+// menu mobile tablet
+
+function handleClickBtnMenuMobile() {
+    const btn_icon = document.querySelector(".header-menu-mobile-icon");
+    const btn_list = document.querySelector(".header-menu-mobile-list");
+    btn_icon.addEventListener("click", () => {
+        btn_list.classList.toggle("active");
+        btn_icon.classList.toggle("fa-xmark");
+    });
+    // btn.onclick = function () {
+    //     document
+    //         .querySelector(".header-menu-mobile-list")
+    //         .classList.toggle("active");
+    // };
+}
+// oncload
+function onLoad() {
+    goToTop();
+    handleClickBtnMenuMobile();
+}
+onLoad();
